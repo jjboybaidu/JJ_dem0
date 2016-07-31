124cd7f0442f52aca4b2f3a53020568803c78bb8
jjboybaidu <jjboybaidu@gmail.com>
2016-08-01 07:45:19 +0800

    会不会有问题

--- JJLogic/JJCLLocation/JJCLLocation/JJCLLocation.m
+++ JJLogic/JJCLLocation/JJCLLocation/JJCLLocation.m
@@ -46,23 +46,22 @@
 
 /*
  
-// 判断获取的位置信息，是不是前15秒前的，而不是缓存里面的。
-// Delegate method from the CLLocationManagerDelegate protocol.
-- (void)locationManager:(CLLocationManager *)manager
-     didUpdateLocations:(NSArray *)locations {
-    // If it's a relatively recent event, turn off updates to save power.
-    CLLocation* location = [locations lastObject];
-    NSDate* eventDate = location.timestamp;
-    NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
-    if (abs(howRecent) < 15.0) {
-        // If the event is recent, do something with it.
-        NSLog(@"latitude %+.6f, longitude %+.6f\n",
-              location.coordinate.latitude,
-              location.coordinate.longitude);
-    }
-}
+ // 判断获取的位置信息，是不是前15秒前的，而不是缓存里面的。
+ // Delegate method from the CLLocationManagerDelegate protocol.
+ - (void)locationManager:(CLLocationManager *)manager
+ didUpdateLocations:(NSArray *)locations {
+ // If it's a relatively recent event, turn off updates to save power.
+ CLLocation* location = [locations lastObject];
+ NSDate* eventDate = location.timestamp;
+ NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
+ if (abs(howRecent) < 15.0) {
+ // If the event is recent, do something with it.
+ NSLog(@"latitude %+.6f, longitude %+.6f\n",
+ location.coordinate.latitude,
+ location.coordinate.longitude);
+ }
+ }
  
-*/
-
+ */
 
 @end
